81ff92ae51ec027bfedc37a2dc0a8bb27775547c
isauraqinthara <isauraqinthara28@gmail.com>
2025-06-25 19:11:45 +0700

    perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,53 @@
 import 'package:flutter/material.dart';
-import 'screen/buyer/my_ticket.dart';
-import 'screen/buyer/home_screen.dart';
-import 'screen/buyer/concert_detail.dart';
-import 'screen/buyer/concert_detail_completed.dart';
+import 'package:fp_festipass/screens/login_screen.dart';
+import 'package:fp_festipass/screens/signup_screen.dart';
+import 'package:fp_festipass/screens/forgot_password_1.dart';
+import 'package:fp_festipass/screens/forgot_password_2.dart';
+import 'package:fp_festipass/screens/forgot_password_3.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
+  static const Color aqua = Color(0xFF22E6CE);
+  static const Color bluishCyan = Color(0xFF21D3FE);
+  static const Color shockingPink = Color(0xFFF51DAB);
+  static const Color orange = Color(0xFFFC8B3A);
+  static const Color lime = Color(0xFF0DD044);
+  static const Color azureBlue = Color(0xFF3479FF);
+  static const Color lovelyPurple = Color(0xFF8D29EE);
+  static const Color yellow = Color(0xFFF1F446);
+
+  static const Color grey1 = Color(0xFF252836);
+  static const Color grey2 = Color(0xFF292E46);
+  static const Color grey3 = Color(0xFF2E3553);
+  static const Color grey4 = Color(0xFFF51DAB);
+  static const Color gradientAquaEnd = Color(0xFF10FF8C);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(
-      backgroundColor: Color(0xFF111317),
-      body: HomeScreen(),
+      title: 'FestiPass',
+      theme: ThemeData(
+        brightness: Brightness.dark, // Overall dark theme
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: grey1, // Set default scaffold background
       ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      // home: HomeScreen(),
-      // home: ConcertDetailScreen(),
-      // home: MyTicketScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // Define routes for easy navigation
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        // Updated route names
+        '/forgot_password_step1': (context) => const ForgotPassword1Screen(),
+        '/forgot_password_step2': (context) => const ForgotPassword2Screen(),
+        '/forgot_password_step3': (context) => const ForgotPassword3Screen(),
+      },
     );
   }
 }
